8c742ea15db29dcc54329b9ab318b6e06c850aa9
Eve Nordinstorm <evenordinstorm@gmail.com>
2024-09-23 18:51:47 +0100

    Initial set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroComponent } from '../components/intro/intro.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { CvComponent } from '../components/cv/cv.component';
+import { ContactComponent } from '../components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroComponent, ProjectsComponent, CvComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
